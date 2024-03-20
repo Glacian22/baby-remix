08953803dba51a16bdb8b18fc12157034f745953
Abram Thau <glacian22@gmail.com>
2024-03-20 14:26:13 -0700

    closing branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import {  BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Routes from "./Routes";
 import { themeAtom } from "./lib/atom";
 import { useAtom } from "jotai"
+import { withLDProvider } from "launchdarkly-react-client-sdk";
 import './App.scoped.css'
 import './App.global.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [theme] = useAtom(themeAtom)
- 
+
   return (
     <div className="app-global" data-theme={theme}>
       <div className=" app-content">
@@ -24,4 +25,9 @@ function App() {
   )
 }
 
-export default App
+export default withLDProvider({
+  clientSideID: '65933e77a9d90b0ffc2b39cc',
+  options: {
+    bootstrap: 'localStorage'
+  }
+})(App)
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,17 @@
 import { motion } from "framer-motion"
 import Button from "../../components/Button"
+import { useFlags } from 'launchdarkly-react-client-sdk'
 import { variants, itemVariants } from "../../lib/anims"
 import "./home.scoped.css"
 
 const Home = () => {
+  const {welcomeText} = useFlags()
+
 
   return (
     <motion.div className='intro' variants={variants} animate='enter' exit='exit' initial='initial'>
-      <motion.div key='explainer' variants={itemVariants}>Here's a whole bunch of intro text telling you how to use the Baby Name Mixer :D :D :D
+      <motion.div key='explainer' variants={itemVariants}>
+        {welcomeText}
       </motion.div>
       <motion.div key='go' variants={itemVariants}>
         <Button to='FirstName'>GO!</Button >
