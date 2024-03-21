0fb0891a08ac446d2d5b12deb88d03a77b7c839c
Abram Thau <glacian22@gmail.com>
2024-03-21 15:59:46 -0700

    added LD flag control for themes, disabling flag removes theme options and reverts to light mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,29 @@
+import { useEffect } from "react"
 import { themeAtom } from "../lib/atom"
 import { useAtom } from "jotai"
+import { useFlags } from 'launchdarkly-react-client-sdk'
 import './navbar.scoped.css'
 
 
 const Navbar = () => {
   const [theme, setTheme] = useAtom(themeAtom)
+  const { enableUiTheme, enableDarkestMode } = useFlags()
+
+  useEffect(() => {
+    if (!enableUiTheme) setTheme('light')
+  }, [enableUiTheme])
+
+  useEffect(() => {
+    if (!enableDarkestMode && theme === 'goth') setTheme('light')
+  }, [enableDarkestMode, theme])
 
   return (
     <nav>
       <div className='app-width'>
         <span>Baby Mix!</span>
-        <button id='theme-btn' onClick={() => setTheme(theme === 'light' ? 'dark' : theme === 'dark' ? 'goth' : 'light')}>{theme}</button>
+        {enableUiTheme &&
+          <button id='theme-btn' onClick={() => setTheme(theme === 'light' ? 'dark' : theme === 'dark' ? 'goth' : 'light')}>{theme}</button>
+        }
       </div>
     </nav>
   )
